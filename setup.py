9d377f4516ba5b8384e45051a6f37f1700a6577d
Ri-Shitetsu <95322390+Ri-Shitetsu@users.noreply.github.com>
2024-01-17 16:13:59 +0900

    Uploaded setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from importlib.metadata import entry_points
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="decom",
+    version="0.0.1",
+    author="Ri Shitetsu",
+    author_email="/////",
+    description="/////",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ri-Shitetsu/Decom",
+    project_urls={
+        "Bug Tracker": "https://github.com/Ri-Shitetsu/Decom",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"":"src"},
+    py_modules=['decom'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    entry_points={
+        'console_scripts':[
+            'decom = decom:main'
+        ]
+    },
+)
+
+
+
+
